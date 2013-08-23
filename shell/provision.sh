f950dc62be6534f28b6667662b9e7f72338f8e00
Gordon Dent <gordonmdent@gmail.com>
2013-08-22 17:40:00 -0700

    install libxml2-dev

--- shell/provision.sh
+++ shell/provision.sh
@@ -6,6 +6,6 @@ npm install -g generator-angular
 \curl -L https://get.rvm.io | bash -s stable --ruby
 source /usr/local/rvm/scripts/rvm
 rvm install 1.9.3
-apt-get install libxml2
+apt-get install libxslt-dev libxml2-dev
 gem install rails
 gem install bundler
