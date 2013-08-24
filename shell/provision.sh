2cd52fb0008f8a329b68e5d8af80bfb1e27d3c04
Gordon Dent <gordonmdent@gmail.com>
2013-08-24 16:36:00 -0700

    make the provisioning slightly more robust - still need more testing

--- shell/provision.sh
+++ shell/provision.sh
@@ -2,10 +2,15 @@
 npm install -g yo
 npm install -g generator-angular
 
-
-\curl -L https://get.rvm.io | bash -s stable --ruby
+apt-get install curl
+curl -L https://get.rvm.io | bash -s stable --ruby
 source /usr/local/rvm/scripts/rvm
 rvm install 1.9.3
-apt-get install libxslt-dev libxml2-dev
+apt-get install libxslt-dev libxml2-dev libxml2
 gem install rails
 gem install bundler
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/10gen.list
+sudo apt-get update
+sudo apt-get install mongodb-10gen
